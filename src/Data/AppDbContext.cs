0b9a908e48eba5420100d0922a29a1b1826a13ba
Rennan Barros <rennan_sbarros@hotmail.com>
2025-05-18 20:20:49 -0300

    feat: ajustando configuração com o BD, conectando e criando tabela para teste com o SQL Server;

---
+++ src/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi_agende_mais.src.Models;
+
+namespace webapi_agende_mais.src.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
